acb5c026e8c28fbba518d5fa6fbc71aef0587aad
Edward Ardian Tayu <edwardtayu4@gmail.com>
2023-05-14 15:50:46 +0700

    Menambahkan layout

--- app/src/main/java/com/example/mechanicalcalculator/MainActivity.java
+++ app/src/main/java/com/example/mechanicalcalculator/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.mechanicalcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView display;
+    private Button btnDelete, btnAnswer, btnAngka1, btnAngka2, btnAngka3, btnAngka4,btnAngka5,
+            btnAngka6, btnAngka7, btnAngka8, btnAngka9, btnAngka0, btnPembagian, btnPengurangan,
+            btnPerkalian, btnPertambahan, btnPersen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
